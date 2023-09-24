a4929894653a08f55e28be71f8b513b68edd044e
Gayas Thasmika <73925625+GT-CodiNgS@users.noreply.github.com>
2023-09-25 01:05:38 +0530

    Removing ignored files

---
+++ EdlerCareApi/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace EdlerCareApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : RESTFulController
+    {
+        [HttpGet]
+        public string GetAllUsers()
+        {
+            return "Hello, Users Here!";
+        }
+
+    }
+}
