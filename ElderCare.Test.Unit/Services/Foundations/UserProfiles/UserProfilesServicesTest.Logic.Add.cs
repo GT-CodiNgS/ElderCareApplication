40a4ceb90cd2b9009b6b21627bfc668320292b69
Gayas Thasmika <73925625+GT-CodiNgS@users.noreply.github.com>
2023-10-13 14:12:51 +0530

    ShouldAddUserProfileAsync --> PASS

---
+++ ElderCare.Test.Unit/Services/Foundations/UserProfiles/UserProfilesServicesTest.Logic.Add.cs
@@ -0,0 +1,41 @@
+﻿using EdlerCareApi.Models.UserProfiles;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElderCare.Test.Unit.Services.Foundations.UserProfiles
+{
+    public partial class UserProfilesServicesTest
+    {
+        [Fact]
+        public async Task ShouldAddUserProfileAsync()
+        {
+            // given
+            UserProfile randomUserProfile = CreateRandomUserProfile();
+            UserProfile inputUserProfile = randomUserProfile;
+            UserProfile storageUserProfile = randomUserProfile;
+            UserProfile expectedUserProfile = storageUserProfile;
+
+            this.storageBrokerMock.Setup(broker =>
+                                      broker.InsertUserProfileAsync(inputUserProfile))
+                    .ReturnsAsync(storageUserProfile);
+
+            // when
+            UserProfile actualUserProfile =
+                await this.userProfileService.AddUserProfileAsync(inputUserProfile);
+
+            // then
+            actualUserProfile.Should().BeEquivalentTo(expectedUserProfile);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserProfileAsync(inputUserProfile),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
