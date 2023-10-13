8c06bcf293e80da75ed2eeb7f627d598244ef573
Gayas Thasmika <73925625+GT-CodiNgS@users.noreply.github.com>
2023-10-13 14:08:44 +0530

    ShouldRetrieveAllUserProfiles --> PASS

---
+++ ElderCare.Test.Unit/Services/Foundations/UserProfiles/UserProfilesServicesTest.Logic.RetriveAll.cs
@@ -0,0 +1,40 @@
+﻿using EdlerCareApi.Models.UserProfiles;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElderCare.Test.Unit.Services.Foundations.UserProfiles
+{
+    public partial class UserProfilesServicesTest
+    {
+        [Fact]
+        public void ShouldRetrieveAllUserProfiles()
+        {
+            // given
+            IQueryable<UserProfile> randomUserProfiles = CreateRandomUserProfiles();
+            IQueryable<UserProfile> storageUserProfiles = randomUserProfiles;
+            IQueryable<UserProfile> expectedUserProfiles = storageUserProfiles;
+
+            this.storageBrokerMock.Setup(broker =>
+                                      broker.SelectAllUserProfiles())
+                    .Returns(storageUserProfiles);
+
+            // when
+            IQueryable<UserProfile> actualUserProfiles =
+                this.userProfileService.RetriveAllActiveUserProfiles();
+
+            // then
+            actualUserProfiles.Should().BeEquivalentTo(expectedUserProfiles);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllUserProfiles(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
