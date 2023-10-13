d186201ff178da16bcc1a0c68e33189026c1d1a9
Gayas Thasmika <73925625+GT-CodiNgS@users.noreply.github.com>
2023-10-13 13:35:17 +0530

    project created

---
+++ ElderCare.Test.Unit/Services/Foundations/Posts/PostServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using EdlerCareApi.Brokers;
+using EdlerCareApi.Models.Post;
+using EdlerCareApi.Services.Foundations.Posts;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace ElderCare.Test.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTest
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IPostService postService;
+
+        public PostServiceTest()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.postService = new PostService(
+                               storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static IQueryable<Post> CreateRandomPosts() =>
+            CreateRandomPosts(count: GetRandomNumber());
+
+        private static int GetRandomNumber() => new IntRange(min: 2, max: 10).GetValue();
+
+        private static IQueryable<Post> CreateRandomPosts(int count) =>
+            CreatePostFiller(dateTime:GetRandomDateTime()).Create(count).AsQueryable();
+
+        private static Filler<Post> CreatePostFiller(DateTimeOffset dateTime)
+        {
+            var filler = new Filler<Post>();
+
+            filler.Setup()
+               .OnType<DateTimeOffset>().Use(dateTime);
+
+            return filler;
+        }
+    }
+}
