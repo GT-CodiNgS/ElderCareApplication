501edb003f02c1a93e1c043187be691c8efce2f9
Gayas Thasmika <73925625+GT-CodiNgS@users.noreply.github.com>
2023-10-13 14:22:14 +0530

    ShouldEditUserProfileAsync -- > PASS

---
+++ ElderCare.Test.Unit/Services/Foundations/UserProfiles/UserProfilesServicesTest.Logic.Edit.cs
@@ -0,0 +1,37 @@
+﻿using EdlerCareApi.Models.UserProfiles;
+using FluentAssertions;
+using Moq;
+
+namespace ElderCare.Test.Unit.Services.Foundations.UserProfiles
+{
+    public partial class UserProfilesServicesTest
+    {
+        [Fact]
+        public async Task ShouldEditUserProfileAsync()
+        {
+            // given
+            UserProfile randomUserProfile = CreateRandomUserProfile();
+            UserProfile inputUserProfile = randomUserProfile;
+            UserProfile storageUserProfile = randomUserProfile;
+            UserProfile expectedUserProfile = storageUserProfile;
+
+            this.storageBrokerMock.Setup(broker =>
+                           broker.UpdateUserProfileAsync(inputUserProfile))
+                    .ReturnsAsync(storageUserProfile);
+
+            // when
+            UserProfile actualUserProfile =
+                await this.userProfileService.ModifyUserProfileAsync(inputUserProfile);
+
+            // then
+            actualUserProfile.Should().BeEquivalentTo(expectedUserProfile);
+
+            this.storageBrokerMock.Verify(broker =>
+                           broker.UpdateUserProfileAsync(inputUserProfile),
+                                               Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+        
+    }
+}
