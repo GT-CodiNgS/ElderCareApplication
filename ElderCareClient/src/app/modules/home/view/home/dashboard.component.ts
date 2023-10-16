234235221fe1525056a157669ef32244453e870c
Gayas Thasmika <73925625+GT-CodiNgS@users.noreply.github.com>
2023-10-16 22:26:43 +0530

    css issues fix

--- ElderCareClient/src/app/modules/home/view/home/dashboard.component.ts
+++ ElderCareClient/src/app/modules/home/view/home/dashboard.component.ts
@@ -1,20 +1,36 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import { Component, OnInit, inject } from '@angular/core';
 import { Post, PostGenderType } from 'src/app/core/models/Post';
 import { PostService } from 'src/app/core/services/post.service';
-import {AddPostComponent} from "../../../add-post/add-post.component";
-import {MatDialog} from "@angular/material/dialog";
-import {ViewPostComponent} from "./inner-components/view-post/view-post.component";
-
+import { AddPostComponent } from '../../../add-post/add-post.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ViewPostComponent } from './inner-components/view-post/view-post.component';
 @Component({
   selector: 'app-home',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('blink', [
+      state('red', style({ color: 'red' })),
+      state('blue', style({ color: 'blue' })),
+      state('green', style({ color: 'green' })),
+      state('yellow', style({ color: 'yellow' })),
+      transition('* <=> *', [animate('1s')]),
+    ]),
+  ],
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   itemsPerPage = 5;
   currentPage = 1;
+  PostGenderType = PostGenderType;
 
-  posts: Post[] = []
+  posts: Post[] = [];
 
   carouselOptions = {
     loop: true,
@@ -22,25 +38,32 @@ export class DashboardComponent implements OnInit{
     nav: true,
     items: 1,
   };
-
+  colorState: string = 'red';
   postService = inject(PostService);
 
-  constructor(public dialog: MatDialog,) {
-  }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.postService.getPosts().subscribe((posts) => {
       this.posts = posts;
+      console.log(posts);
     });
+    let colors: string[] = ['red', 'blue', 'green', 'yellow'];
+    let index = 0;
+
+    setInterval(() => {
+      this.colorState = colors[index % colors.length];
+      index++;
+    }, 800);
   }
 
-  viewPost(post:Post) {
+  viewPost(post: Post) {
     let dialogRef = this.dialog.open(ViewPostComponent, {
       width: '600px',
       height: 'auto',
       panelClass: 'model-preview',
       hasBackdrop: true,
-      data:post
+      data: post,
     });
   }
 }
