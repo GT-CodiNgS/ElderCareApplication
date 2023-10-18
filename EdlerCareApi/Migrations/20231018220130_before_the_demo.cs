23b174fe244746b1b6653ec501763c7bd28835d3
Kalinga <kalingaru48@gamil.com>
2023-10-19 03:39:37 +0530

    feat: adding new migrations

---
+++ EdlerCareApi/Migrations/20231018220130_before_the_demo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EdlerCareApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class before_the_demo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
